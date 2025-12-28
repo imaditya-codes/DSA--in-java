e4d71019a3dce3e5fc9bb37c59ca237021b89a22
imaditya-codes <imaditya.cse@gmail.com>
2025-12-29 02:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (32.89%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n=nums.length;
+
+        //handling the edge cases
+        if(n==1){
+            return nums[0];
+        }
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+
+        int  low=1;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+
+            //eliminate the left half
+            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
+                low=mid+1;
+            }
+            
+            //eliminate the right half
+            else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
