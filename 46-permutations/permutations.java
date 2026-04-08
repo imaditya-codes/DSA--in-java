932b8171c4a0f28c12ce2dd1896e9ac48a9d2ae2
ADITYA KUMAR <imaditya.cse@gmail.com>
2026-04-09 02:38:04 +0530

    Time: 1 ms (85.20%) | Memory: 45.4 MB (69.74%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ds=new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        recurPermute(nums,ds,ans,used);
+        return ans;
+
+    }
+
+    public void recurPermute(int[] nums, List<Integer> ds , List<List<Integer>> ans , boolean[] used ){
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            if(!used[i]){
+                used[i]=true;
+                ds.add(nums[i]);
+                recurPermute(nums,ds,ans,used);
+                ds.remove(ds.size()-1);
+                used[i]=false;
+            }
+        }
+
+    }
+}
