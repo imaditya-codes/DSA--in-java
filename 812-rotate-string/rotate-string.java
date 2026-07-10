0e43e45eafa4586ebc86d05455f6250a72a7b558
ADITYA KUMAR <imaditya.cse@gmail.com>
2026-07-10 12:27:52 +0530

    Time: 2 ms (29.00%) | Memory: 43.4 MB (19.67%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,13 +3,8 @@ class Solution {
         if(s.length()!=goal.length()){
             return false;
         }
-        // String res=s+s;
-        // return res.contains(goal);
-        String temp= s;
-        for(int i=0;i<s.length();i++){
-            temp=temp.substring(1)+temp.charAt(0);
-            if(temp.equals(goal)) return true;
-        }
-        return s.equals(goal);
+        String res=s+s;
+        return res.contains(goal);
+    
     }
 }
