f977659bf855984420533e869463db9910310159
ADITYA KUMAR <imaditya.cse@gmail.com>
2026-07-11 12:25:00 +0530

    Time: 17 ms (16.66%) | Memory: 46.5 MB (33.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,16 +3,21 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int [] freq = new int[26];
+        HashMap<Character,Integer> map= new HashMap<>();
         for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for(int count: freq){
-            if(count!=0){
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(!map.containsKey(ch)){
                 return false;
             }
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch)==0){
+                map.remove(ch);
+            }
         }
-        return true;
+        return map.isEmpty();
     }
 }
