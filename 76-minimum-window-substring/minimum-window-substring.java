91bbed7093e1fd4d5bf54df34df8fb4d9f8a2036
ADITYA KUMAR <imaditya.cse@gmail.com>
2026-08-15 16:50:57 +0530

    Time: 3 ms (72.87%) | Memory: 45.3 MB (94.28%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,39 +1,53 @@
 class Solution {
     public String minWindow(String s, String t) {
+
         int minlen = Integer.MAX_VALUE;
         int l = 0;
         int r = 0;
+
         int n = s.length();
         int m = t.length();
+
         int count = 0;
         int start = -1;
 
-        HashMap<Character , Integer > map = new HashMap<>();
-        for(char c : t.toCharArray()){
-            map.put(c,map.getOrDefault(c , 0)+1);
+        int[] freq = new int[256];
+
+        for (char c : t.toCharArray()) {
+            freq[c]++;
         }
-        while(r<n){
-            if(map.containsKey(s.charAt(r)) && map.get(s.charAt(r)) > 0){
+
+        while (r < n) {
+
+            if (freq[s.charAt(r)] > 0) {
                 count++;
             }
-            map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0)-1);
-            while(count == m){
-                 if (r - l + 1 < minlen) {
+
+            freq[s.charAt(r)]--;
+
+            while (count == m) {
+
+                if (r - l + 1 < minlen) {
                     minlen = r - l + 1;
                     start = l;
                 }
-                 map.put(s.charAt(l),map.getOrDefault(s.charAt(l),0)+1);
-                 if (map.get(s.charAt(l)) > 0) {
-                        count--;
-                    }
+
+                freq[s.charAt(l)]++;
+
+                if (freq[s.charAt(l)] > 0) {
+                    count--;
+                }
+
                 l++;
             }
+
             r++;
         }
-         if (start == -1) {
+
+        if (start == -1) {
             return "";
         }
 
         return s.substring(start, start + minlen);
     }
-} 
+}
